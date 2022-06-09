463126a4aed1f20bac718d2702624dd330c68c7c
Cameron Griffin <cameron.griffin@jupiterone.com>
2022-06-09 10:21:54 -0400

    : Added stdio to execSync
    Added stdio inherit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-
+const { stringify } = require('querystring');
+const npm_auth_token = core.getInput('npm_auth_token', { required: true });
+const gh_token = core.getInput('gh_token', { required: true });
 
 const main = async () => {
+
+  const execSync = require('child_process').execSync;
+  
   try {
-
-    //Get current version
-    const { exec } = require("child_process");
-    
-
-    exec("yarn version --patch", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        const currentVersion = stdout;
-        core.setOutput("currentVersion", currentVersion);
-    });
-    //bump
-
-    //Push new version
-
-    
+    execSync('yarn global add auto',
+      {stdio: 'inherit'})
+    execSync('yarn install',
+      {stdio: 'inherit'})
+    execSync('yarn build',
+      {stdio: 'inherit'})
+    execSync('git config user.email "internal-automation.bot@jupiterone.com"',
+      {stdio: 'inherit'})
+    execSync('git config user.email "internal-automation.bot@jupiterone.com"',
+      {stdio: 'inherit'})
+    execSync('git config user.name "j1-internal-automation"',
+      {stdio: 'inherit'})
+    execSync('git config --global commit.verbose true',
+      {stdio: 'inherit'})
+    execSync('git fetch --tags',
+      {stdio: 'inherit'})
+    execSync(`echo "//registry.npmjs.org/:_authToken=${npm_auth_token}" > .npmrc`,
+      {stdio: 'inherit'})
+    execSync(`NPM_TOKEN=${npm_auth_token} GH_TOKEN=${gh_token} auto shipit --email internal-automation.bot@jupiterone.com --name j1-internal-automation`,
+      {stdio: 'inherit'})
   }
   catch (error) {
     core.setFailed(error.message);
